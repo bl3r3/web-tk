bf69380348c9c628ec9f6c093168908ca74e6e77
Domingo Cabeza <domingoacd1@gmail.com>
2019-05-22 18:58:44 -0400

    Agregado efecto scroll en el nav

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('nav:not(.navbar--index)');
+
+function manageScroll() {
+    if (window.pageYOffset > 100) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+}
+
+window.addEventListener('scroll', manageScroll);
