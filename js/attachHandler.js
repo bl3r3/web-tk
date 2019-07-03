0e4b6867c49eedd975a8573d0f325f5b219148e2
Domingo Cabeza <domingoacd1@gmail.com>
2019-07-03 12:03:37 -0400

    Corregido estilo del topic select

--- js/attachHandler.js
+++ js/attachHandler.js
@@ -9,3 +9,8 @@ attachInput.addEventListener('change', (event) => {
         attachStyler.innerText = 'Attach a file';
     }
 });
+
+const select = document.querySelector('select[name="subject"]');
+select.addEventListener('change', (event) => {
+    select.classList.add('selected');
+})
