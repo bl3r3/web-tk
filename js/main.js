5c4b2a7823360112ec0ee06825a703de6b10e6b4
racg21 <carrasquel18@gmail.com>
2019-07-30 11:49:07 -0400

    cambios en el navbar

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@
 // }
 
 // window.addEventListener('scroll', manageScroll);
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
