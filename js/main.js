e42ec70c1f67f03f1257c32acf09a5efbe067c50
Domingo Cabeza <domingoacd1@gmail.com>
2019-08-05 10:23:30 -0400

    Prueba de scroll

--- js/main.js
+++ js/main.js
@@ -10,13 +10,39 @@
 
 // window.addEventListener('scroll', manageScroll);
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-100px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   const navTop = document.getElementById("navbar").getBoundingClientRect().top;
+//   const navBottom = document.getElementById("navbar").getBoundingClientRect().bottom;
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-" + (navBottom - navTop) + "px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+let prevScrollpos = window.pageYOffset;
+window.addEventListener('scroll', () => {
+  const navTop = document.getElementById("navbar").getBoundingClientRect().top;
+  const navBottom = document.getElementById("navbar").getBoundingClientRect().bottom;
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-100px";
+    document.getElementById("navbar").style.top = "-" + (navBottom - navTop) + "px";
   }
   prevScrollpos = currentScrollPos;
-}
+}); 
